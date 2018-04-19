27237ab2eee78d2708665b3d0c1304b6f11c03f1
utnso <dantebado@gmail.com>
2018-04-19 16:41:07 -0300

    Proyecto de Eclipse con commons incluidas

---
+++ src/headers.h
@@ -0,0 +1,8 @@
+#ifndef SRC_HEADERS_H_
+#define SRC_HEADERS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <commons/log.h>
+
+#endif /* SRC_HEADERS_H_ */
---
+++ src/launcher.c
@@ -0,0 +1,7 @@
+#include "headers.h"
+
+int main() {
+	printf("SISTEMAS OPERATIVOS");
+
+	return 1;
+}

95b954cad42f5233aaf7266c58184ad8188697dd
npyt <ramaranzana@est.frba.utn.edu.ar>
2018-04-28 17:59:55 -0300

    Creado el proyecto del proceso ESIs

---
+++ esi/launcher.c
@@ -0,0 +1,11 @@
+#include "headers.h"
+
+t_log * logger;
+t_config * config;
+
+int main() {
+	logger = log_create("esi_logger.log", "ESI", true, LOG_LEVEL_TRACE);
+	config = config_create("esi_config.cfg");
+
+	return 0;
+}

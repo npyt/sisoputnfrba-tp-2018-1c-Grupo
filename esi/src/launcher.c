740025d39bb1471c3c1287e4bb67076f933ae20d
utnso <lucasrango@est.frba.utn.edu.ar>
2018-05-24 21:25:37 -0300

    Implementacion del parser de la catedra

--- esi/src/launcher.c
+++ esi/src/launcher.c
@@ -1,11 +1,53 @@
 #include "libgrupo/headers.h"
+#include <parsi/parser.h>
 
-t_log * logger;
-t_config * config;
+int main(int argc, char **argv){
+    FILE * fp;
+    char * line = NULL;
+    size_t len = 0;
+    ssize_t read;
 
-int main() {
+	t_log * logger;
+	t_config * config;
+
 	logger = log_create("esi_logger.log", "ESI", true, LOG_LEVEL_TRACE);
 	config = config_create("esi_config.cfg");
 
-	return 0;
+    fp = fopen(argv[1], "r");
+    if (fp == NULL){
+        perror("Error al abrir el archivo: ");
+        exit(EXIT_FAILURE);
+    }
+
+    while ((read = getline(&line, &len, fp)) != -1) {
+    	t_esi_operacion parsed = parse(line);
+
+        if(parsed.valido){
+            switch(parsed.keyword){
+                case GET:
+                    printf("GET\tclave: <%s>\n", parsed.argumentos.GET.clave);
+                    break;
+                case SET:
+                    printf("SET\tclave: <%s>\tvalor: <%s>\n", parsed.argumentos.SET.clave, parsed.argumentos.SET.valor);
+                    break;
+                case STORE:
+                    printf("STORE\tclave: <%s>\n", parsed.argumentos.STORE.clave);
+                    break;
+                default:
+                    fprintf(stderr, "No pude interpretar <%s>\n", line);
+                    exit(EXIT_FAILURE);
+            }
+
+            destruir_operacion(parsed);
+        } else {
+            fprintf(stderr, "La linea <%s> no es valida\n", line);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    fclose(fp);
+    if (line)
+        free(line);
+
+    return EXIT_SUCCESS;
 }

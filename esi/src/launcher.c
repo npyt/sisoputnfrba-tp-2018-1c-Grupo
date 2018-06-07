54547f02b237d4f1f9922377fa53b89616d28937
npyt <ramaranzana@est.frba.utn.edu.ar>
2018-06-06 23:05:47 -0300

    Modifico listening
    Utilizo select()

--- esi/src/launcher.c
+++ esi/src/launcher.c
@@ -4,23 +4,28 @@
 t_log * logger;
 FILE * fp;
 
-int coordinator_socket;
-int planner_socket;
+typedef struct {
+	int planner_socket;
+	int coord_socket;
+} SocketToListen;
+
+void * listening_threads(SocketToListen*);
 
 int main(int argc, char **argv){
+	// CONFIG
 	fp = fopen(argv[1], "r");
 //	if (fp == NULL){
 //		perror("Error al abrir el archivo: ");
 //		exit(EXIT_FAILURE);
 //	}
 
-
 	t_config * config;
-
 	logger = log_create("esi_logger.log", "ESI", true, LOG_LEVEL_TRACE);
 	config = config_create("esi_config.cfg");
+	// END CONFIG
+
 	// COORD CONECTION
-	coordinator_socket = connect_with_server(config_get_string_value(config, "IP_COORD"),
+	int coordinator_socket = connect_with_server(config_get_string_value(config, "IP_COORD"),
 			atoi(config_get_string_value(config, "PORT_COORD")));
 	if (coordinator_socket < 0){
 		log_error(logger, " ERROR AL CONECTAR CON EL COORDINADOR");
@@ -33,35 +38,32 @@ int main(int argc, char **argv){
 	// END COORD CONNECTION
 
 	// PLANNER CONNECTION
-	planner_socket = connect_with_server(config_get_string_value(config, "IP_PLAN"),
+	int planner_socket = connect_with_server(config_get_string_value(config, "IP_PLAN"),
 			atoi(config_get_string_value(config, "PORT_PLAN")));
 	if (planner_socket < 0){
 		log_error(logger, " ERROR AL CONECTAR CON EL PLANIFICADOR");
 		return -1;
 	} else {
-		log_info(logger, " CONECTADO EN: %d",planner_socket);
+		log_info(logger, " CONECTADO EN: %d", planner_socket);
 	}
 	log_info(logger, "Envío saludo al planificador");
 	send_only_header(planner_socket, ESI_PLANNER_HANDSHAKE);
 	// END PLANNER CONNECTION
 
 	// LISTENING THREAD
-
-
-	pthread_t listening_thread_coordinator;
-	pthread_create(&listening_thread_coordinator, NULL, coordinator_listening_thread);
-
-	pthread_t listening_thread_planner;
-	pthread_create(&listening_thread_planner, NULL, planner_listening_thread);
-
-
+	SocketToListen * sockets_to_listen = malloc(sizeof(SocketToListen));
+	sockets_to_listen->coord_socket = coordinator_socket;
+	sockets_to_listen->planner_socket = planner_socket;
+	pthread_t listening_thread_id;
+	pthread_create(&listening_thread_id, NULL, listening_threads, sockets_to_listen);
 	// END LISTENING THREAD
 
+	pthread_exit(NULL);
 	fclose(fp);
     return EXIT_SUCCESS;
 }
 
-void parser(int coordinator_socket){
+void parser(int coordinator_socket, int planner_socket){
     char * line = NULL;
     size_t len = 0;
     ssize_t read;
@@ -124,72 +126,80 @@ void parser(int coordinator_socket){
 		exit(EXIT_SUCCESS);
 }
 
-void * coordinator_listening_thread() {
-
-	while(1) {
-		MessageHeader * header = malloc(sizeof(MessageHeader));
-		int rec = recv(coordinator_socket, header, sizeof(MessageHeader), 0);
-		//Procesar el resto del mensaje dependiendo del tipo recibido
-		switch((*header).type) {
-			case COORD_ESI_EXECUTED:
-				send_only_header(planner_socket, ESI_EXECUTION_LINE_OK);
-				break;
-			case OPERATION_ERROR:
-				log_info(logger, "El COORDINADOR me informa de ERROR");
-				send_only_header(planner_socket, ESI_EXECUTION_FINISHED);
-				fflush(stdout);
-				exit(EXIT_FAILURE);
-				break;
-			case ESI_COORD_HANDSHAKE_OK:
-				log_info(logger, "El COORDINADOR aceptó mi conexión");
-				fflush(stdout);
-				break;
-			case UNKNOWN_MSG_TYPE:
-				log_error(logger, "[MY_MESSAGE_HASNT_BEEN_DECODED]");
-				break;
-			default:
-				log_error(logger, "[UNKNOWN_MESSAGE_RECIEVED]");
-				send_only_header(coordinator_socket, UNKNOWN_MSG_TYPE);
-				break;
+void * listening_threads(SocketToListen * socket_to_listen){
+	int coordinator_socket = socket_to_listen->coord_socket;
+	int planner_socket = socket_to_listen->planner_socket;
+	fd_set master;
+	FD_ZERO(&master);
+	FD_SET(coordinator_socket, &master);
+	FD_SET(planner_socket, &master);
+	struct timeval tv;
+	int returning;
+	while(1){
+		returning = select(coordinator_socket+planner_socket+1, &master, NULL, NULL, NULL);
+		if(returning < 0){
+			log_error(logger, " ERRORRRR");
+			return -1;
+		}else{
+		if(FD_ISSET(coordinator_socket, &master)){
+			MessageHeader * header = malloc(sizeof(MessageHeader));
+			recv(coordinator_socket, header, sizeof(MessageHeader), 0);
+			switch((*header).type){
+				case ESI_COORD_HANDSHAKE_OK:
+					log_info(logger, "El COORDINADOR aceptó mi conexión");
+					fflush(stdout);
+					break;
+				case UNKNOWN_MSG_TYPE:
+					log_error(logger, "[MY_MESSAGE_HASNT_BEEN_DECODED]");
+					fflush(stdout);
+					break;
+				case OPERATION_ERROR:
+					log_info(logger, "Me informan de ERROR");
+					send_only_header(coordinator_socket, ESI_EXECUTION_FINISHED);
+					// Terminar ejecución
+					fflush(stdout);
+					break;
+				default:
+					log_error(logger, "[UNKOWN_MESSAGE_RECIEVED]");
+					send_only_header(coordinator_socket, UNKNOWN_MSG_TYPE);
+					fflush(stdout);
+					break;
+			}
 		}
-		free(header);
-	}
-}
+		if(FD_ISSET(planner_socket, &master)){
+			MessageHeader * header = malloc(sizeof(MessageHeader));
+			recv(planner_socket, header, sizeof(MessageHeader), 0);
+			switch((*header).type) {
+				case PLANNER_ESI_RUN:
+					log_info(logger, "[RUNNING_OK__PARSING_NEW_SENTENCE");
+					parser(coordinator_socket, planner_socket);
+					fflush(stdout);
+					break;
+				case ESI_PLANNER_HANDSHAKE_OK:
+					log_info(logger, "El PLANIFICADOR aceptó mi conexión");
 
-void * planner_listening_thread() {
+					// ========== RECV STRUCTURE REGISTRATION VARIATION ==========
+					//ESIRegistration * esi_name = malloc(sizeof(ESIRegistration));
+					//recv(server_socket, esi_name, sizeof(ESIRegistration), 0);
+					//log_info(logger, "[REGISTERED_AS_%s]", esi_name->id);
+					// ========== END RECV STRUCTURE REGISTRATION VARIATION ==========
 
-	while(1) {
-		MessageHeader * header = malloc(sizeof(MessageHeader));
-		int rec = recv(planner_socket, header, sizeof(MessageHeader), 0);
-		//Procesar el resto del mensaje dependiendo del tipo recibido
-		switch((*header).type) {
-			case PLANNER_ESI_RUN:
-				log_info(logger, "[RUNNING_OK__PARSING_NEW_SENTENCE");
-				parser(coordinator_socket);
-				fflush(stdout);
-				break;
-			case ESI_PLANNER_HANDSHAKE_OK:
-				log_info(logger, "El PLANIFICADOR aceptó mi conexión");
-
-				// ========== RECV STRUCTURE REGISTRATION VARIATION ==========
-				//ESIRegistration * esi_name = malloc(sizeof(ESIRegistration));
-				//recv(server_socket, esi_name, sizeof(ESIRegistration), 0);
-				//log_info(logger, "[REGISTERED_AS_%s]", esi_name->id);
-				// ========== END RECV STRUCTURE REGISTRATION VARIATION ==========
-
-				char * esi_buffer_name[ESI_NAME_MAX_SIZE];
-				recv(planner_socket, esi_buffer_name, sizeof(ESI_NAME_MAX_SIZE), 0);
-				log_info(logger, "[REGISTERED_AS_%s]", esi_buffer_name);
-				fflush(stdout);
-				break;
-			case UNKNOWN_MSG_TYPE:
-				log_error(logger, "[MY_MESSAGE_HASNT_BEEN_DECODED]");
-				break;
-			default:
-				log_error(logger, "[UNKNOWN_MESSAGE_RECIEVED]");
-				send_only_header(coordinator_socket, UNKNOWN_MSG_TYPE);
-				break;
+					char * esi_buffer_name[ESI_NAME_MAX_SIZE];
+					recv(planner_socket, esi_buffer_name, sizeof(ESI_NAME_MAX_SIZE), 0);
+					log_info(logger, "[REGISTERED_AS_%s]", esi_buffer_name);
+					fflush(stdout);
+					break;
+				case UNKNOWN_MSG_TYPE:
+					log_error(logger, "[MY_MESSAGE_HASNT_BEEN_DECODED]");
+					fflush(stdout);
+					break;
+				default:
+					log_error(logger, "[UNKOWN_MESSAGE_RECIEVED]");
+					send_only_header(planner_socket, UNKNOWN_MSG_TYPE);
+					fflush(stdout);
+					break;
+			}
 		}
-		free(header);
+		}
 	}
 }
